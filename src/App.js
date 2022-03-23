3e9ba6865dede7fc62546727b3690fb7038c2e52
sanket <lakhtriyasanket@gmail.com>
2022-03-23 13:22:40 +0530

    new change updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Button, DatePicker, version } from "antd";
 import './App.css';
 import "antd/dist/antd.css";
 import LoginForm from "./LoginForm";
+import HelloWorld from "./HelloWorld";
 import Todo from "./todo";
+
 
 function App() {
   return (
-     <Todo/>
+     <HelloWorld/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Todo from "./todo";
-import HelloWorld from "/HelloWorld";
 import "./style.css";
 
 function App() {
     return (
-        <div className="App">
-            <Todo />
-            <HelloWorld/>
-        </div>
-    );
+          <div>
+            <h1>good morning new data applied</h1>
+          </div>
+        );
 }
 
 const rootElement = document.getElementById("root");
