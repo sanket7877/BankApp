c73fd07d7e0169d9eb9ac7f13f96ba5cb192bf7c
sanket <lakhtriyasanket@gmail.com>
2022-03-24 21:41:51 +0530

    code added

---
+++ src/signup/signupform.js
@@ -0,0 +1,12 @@
+function SignUpForm() {
+    return (
+      <div>
+        <h1>yews</h1>
+        <h2>welcome to git</h2>
+        <h3>good morning</h3>
+        <h4>nice</h4>
+      </div>
+    );
+  }
+  
+  export default SignUpForm;
