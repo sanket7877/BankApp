4dd3be55ea84673c3dac71b8b9f3e20ee438e9ff
sanket <lakhtriyasanket@gmail.com>
2022-03-26 23:23:37 +0530

    add antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 import SignUpForm from './signup/signupform';
 
 function App() {
   return (
     <div className="App">
       <SignUpForm/>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
